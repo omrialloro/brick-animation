07f54d43398aa94ae9fee64b23cd773566205911
omrialloro <omrialloro@gmail.com>
2021-09-30 22:27:28 +0300

    fix

--- tst.js
+++ tst.js
@@ -3,6 +3,5 @@ var port = 9000;
 http.createServer(function (req,res){
   res.writeHead(200,{'Content-Type':'text/plain'});
   res.end('Hello World\n')
-
-}).lister(port)
+}).listen(port)
 console.log('Listening on port', port)
