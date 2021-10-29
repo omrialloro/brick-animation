9d7b68ed7795a2ff3281935e808521376549cc43
omrialloro <omrialloro@gmail.com>
2021-10-29 13:59:41 +0300

    500

--- tst.js
+++ tst.js
@@ -14,7 +14,7 @@ const fs = require('fs')
 app = express()
 
 app.listen(5000)
-console.log("listening to port 3000")
+console.log("listening to port 5000")
 
 app.get('/check',function (req,res){
   console.log("running")
